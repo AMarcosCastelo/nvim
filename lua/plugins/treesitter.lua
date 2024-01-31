bd2af947ed43180e77e66b9fa0ea7b7d202874fc
AMarcosCastelo <marcos-12marcos@hotmail.com>
2024-01-31 09:07:19 -0300

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,30 @@
 return {
-  'nvim-treesitter/nvim-treesitter'
+  'nvim-treesitter/nvim-treesitter',
+  build = "TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        'lua',
+        'markdown',
+        'json',
+        'javascript',
+        'typescript',
+        'yaml',
+        'html',
+        'css',
+        'bash',
+        'dockerfile',
+        'gitignore',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighthing = true
+      }
+    })
+  end
+
 }
